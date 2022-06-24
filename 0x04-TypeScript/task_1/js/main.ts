3e5a449618b020635f6bb50c98dd8070ce775462
elayneh <betenatefera@gmail.com>
2022-06-24 22:32:12 +0300

    file for task_1

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,18 +1,47 @@
-interface Techer {
-    readonly firstName: string;
-    readonly lastName: string;
-    fulltimeEmployee: boolean;
-    yearsOfExperience?: number;
-    location: string;
-    [ifAny: string]: any;
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fulltimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [ifAny: string]: any;
 }
 
-const teacher3: Techer = {
-    firstName: 'John',
-    fulltimeEmployee: false,
-    lastName: 'Doe',
-    location: 'Addis Ababa',
-    contract: false,
+interface Directors extends Teacher {
+  numberOfReports: number;
 }
 
-console.log(teacher3['firstName']);
+function printTeacher(firstName: string, lastName: string): string {
+  return firstName[0] + "." + lastName;
+}
+
+interface mixedInteface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+//The class should be described through an interface
+class StudentClass implements mixedInteface {
+  firstName: string;
+  lastName: string;
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  workOnHomework(): string {
+    return "Currently working";
+  }
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+//The constructor of the class should be described through an Interface
+interface constructor {
+  new (firstName: string, lastName: string): mixedInteface;
+}
+
+const student1: StudentClass = new StudentClass("belayneh", "Getachew");
+console.log(student1.displayName());
