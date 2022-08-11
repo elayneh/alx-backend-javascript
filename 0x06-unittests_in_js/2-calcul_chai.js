7cb19e57b10ba6525ed3ce56d75beb0a53cd58d0
elayneh <betenatefera@gmail.com>
2022-08-11 13:54:13 +0300

    corrected

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,7 +1,6 @@
-const calcuteNumber = (type, a, b) => {
+function calculateNumber(type, a, b) {
     if (type === 'SUM') return Math.round(a) + Math.round(b);
     if (type === 'SUBTRACT') return Math.round(a) - Math.round(b);
     if (type === 'DIVIDE') return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
-
-};
-module.exports = calcuteNumber;
+}
+module.exports = calculateNumber;
